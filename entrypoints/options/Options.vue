32bed1b2747c6307b841a30c3512a1f2433a4333
hicode0101 <hicode0101@qq.com>
2024-10-25 23:41:43 +0800

    nice day!

--- entrypoints/options/Options.vue
+++ entrypoints/options/Options.vue
@@ -11,22 +11,36 @@
                 <n-tabs :bar-width="28" type="line" class="custom-tabs">
                     <n-tab-pane name="tabConfigs" tab="Proxy Configs">
 
-                        <n-dynamic-input v-model:value="customValue" :on-create="onCreate">
+                        <n-dynamic-input v-model:value="proxyConfigList" :on-create="onCreateConfig">
                             <template #create-button-default>
-                                随便搞点啥
+                                新增代理配置
                             </template>
                             <template #default="{ value }">
                                 <div style="display: flex; align-items: center; width: 90%;padding-left: 20px;">
-                                    
-                                    <n-input v-model:value="value.pname" type="text" :style="{ width: '33%' }" />
+                                    <n-color-picker v-model:value="value.color" :modes="['hex']" :show-alpha="false" :style="{ width: '90px' }" />
+                                    <n-input v-model:value="value.pname" type="text" placeholder="代理配置的名称，例如：http-8080，方便自己区分" :style="{ width: '33%' }" />
                                     <n-select v-model:value="value.scheme" :style="{ width: '120px' }" :options="proxyTypeOptions" />
                                     
                                     
-                                    <n-input v-model:value="value.host" type="text" :style="{ width: '300px' }"/>
-                                    <n-input v-model:value="value.port" type="text" :style="{ width: '150px' }"/>
+                                    <n-input v-model:value="value.host" type="text" placeholder="host" :style="{ width: '300px' }"/>
+                                    <n-input v-model:value="value.port" type="text" placeholder="port" :style="{ width: '150px' }"/>
                                 </div>
                             </template>
                         </n-dynamic-input>
+
+                        <n-divider />
+
+                        <div class="btnBlock">
+                            <n-space>
+                                <n-button type="primary" @click="saveConfigs">
+                                保存
+                                </n-button>
+                                <n-button strong secondary type="error" @click="resetConfigs">
+                                重置
+                                </n-button>
+                            </n-space>
+                        </div>
+
 
                     </n-tab-pane>
                     <n-tab-pane name="tabAbout" tab="About">
@@ -67,6 +81,10 @@
 </template>
 
 <script lang="ts" setup>
+import { defineComponent, h, reactive, ref } from 'vue'
+import { NIcon, useMessage } from 'naive-ui'
+
+const message = useMessage()
 
 function openPopup() {
     window.location.href = "./popup.html";
@@ -76,18 +94,12 @@ function openOptions() {
     window.location.href = "./options.html";
 }
 
-let customValue = ref([
-    {
-        pname: 'http-8080',
-        scheme: "HTTP",
-        host: "127.0.0.1",
-        port: "8080"
-    }
-]);
 
-function onCreate() {
+
+function onCreateConfig() {
     return {
         pname: 'http-8080',
+        color: '#9F9F9F',
         scheme: "HTTP",
         host: "127.0.0.1",
         port: "8080"
@@ -103,7 +115,51 @@ let proxyTypeOptions = ['HTTP', 'HTTPS', 'SOCKS4', 'SOCKS5'].map(
         })
       );
 
-      
+let proxyConfigList = ref([
+    {
+        pname: 'http-8080',
+        color: '#9F9F9F',
+        scheme: "HTTP",
+        host: "127.0.0.1",
+        port: "8080"
+    }
+]);
+
+
+browser.storage.local.get(["proxyConfigs"]).then((result: any) => {
+    console.log("renderProxyConfigList-1", proxyConfigList);
+    console.log("GetValue is ", result);
+    if (result.proxyConfigs == undefined) {
+        console.log("GetValue is undefined");
+        
+    } else {
+        let proxyConfigs = new Map(JSON.parse(result.proxyConfigs));
+        console.log("map is ", proxyConfigs);
+        proxyConfigs.forEach((proxyConfig, key) => {
+            console.log(key + " = " + proxyConfig);
+            proxyConfigList.value.push({
+                pname: proxyConfig.pid,
+                color: proxyConfig.color,
+                scheme: proxyConfig.rules.singleProxy.scheme.toUpperCase(),
+                host: proxyConfig.rules.singleProxy.host,
+                port: proxyConfig.rules.singleProxy.port.toString()
+            });
+        });
+
+        
+    }
+    
+    
+
+});   
+
+function saveConfigs() {
+    message.info("功能继续完善中...");
+}
+
+function resetConfigs() {
+    message.info("reset 功能继续完善中...");
+}
 
 </script>
 
@@ -118,6 +174,11 @@ let proxyTypeOptions = ['HTTP', 'HTTPS', 'SOCKS4', 'SOCKS5'].map(
 
     background-color: rgba(244, 244, 244, 0.24);
     min-height: 60vh;
+}
+
+.btnBlock {
+    height: 80px;
+    place-items: center;
 }
 
 .bottomBlock {
