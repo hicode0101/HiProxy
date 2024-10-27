0bd64a78451b9b7afdc6e09ff31a1b8b5051b5cf
hicode0101 <hicode0101@qq.com>
2024-10-27 17:31:41 +0800

    完成 配置保存功能（Complete the configuration save function）

--- entrypoints/options/Options.vue
+++ entrypoints/options/Options.vue
@@ -17,13 +17,27 @@
                             </template>
                             <template #default="{ value }">
                                 <div style="display: flex; align-items: center; width: 90%;padding-left: 20px;">
-                                    <n-color-picker v-model:value="value.color" :modes="['hex']" :show-alpha="false" :style="{ width: '90px' }" />
-                                    <n-input v-model:value="value.name" type="text" placeholder="代理配置的名称，例如：http-8080，方便自己区分" :style="{ width: '33%' }" />
-                                    <n-select v-model:value="value.rules.singleProxy.scheme" :style="{ width: '120px' }" :options="proxyTypeOptions" />
+                                    <n-button ghost v-model:color="value.color">
+                                    <template #icon>
+                                        <n-icon>
+                                        <ChromeIcon />
+                                        </n-icon>
+                                    </template>
+                                    </n-button>
+                                    <n-color-picker v-model:value="value.color" :modes="['hex']" :show-alpha="false" :style="{ width: '90px',margin: '0 5px'  }" :actions="['confirm']" />
+                                    <n-input v-model:value="value.name" @change="changeNameEvent(value,$event)" @keyup="keyUpNameEvent(value,$event)" type="text" placeholder="代理配置的名称，例如：http-8080，方便自己区分" :style="{ width: '33%',margin: '0 5px' }" />
+                                    <n-select v-model:value="value.rules.singleProxy.scheme" :style="{ width: '120px',margin: '0 5px' }" :options="proxyTypeOptions" />
                                     
                                     
-                                    <n-input v-model:value="value.rules.singleProxy.host" type="text" placeholder="host" :style="{ width: '300px' }"/>
-                                    <n-input-number v-model:value="value.rules.singleProxy.port" :show-button="false" :style="{ width: '150px' }"/>
+                                    <n-input v-model:value="value.rules.singleProxy.host" type="text" placeholder="host" :style="{ width: '300px',margin: '0 5px' }"/>
+                                    <n-input-number v-model:value="value.rules.singleProxy.port" :show-button="false" :style="{ width: '150px',margin: '0 5px' }"/>
+                                    
+                                    <n-button tertiary type="info" @click="editDetail(value,$event)">
+                                    <template #icon>
+                                        <n-icon><DetailIcon /></n-icon>
+                                    </template>
+                                    </n-button>
+    
                                 </div>
                             </template>
                         </n-dynamic-input>
@@ -33,11 +47,12 @@
                         <div class="btnBlock">
                             <n-space>
                                 <n-button type="primary" @click="saveConfigs">
-                                保存
+                                批量保存
                                 </n-button>
                                 <n-button strong secondary type="error" @click="resetConfigs">
                                 重置
                                 </n-button>
+                                
                             </n-space>
                         </div>
 
@@ -73,19 +88,118 @@
         </n-gi>
         <n-gi>
             <div class="bottomBlock">
-                Copyright by hicode0101
+                Copyright by <a href="https://github.com/hicode0101" target="_blank">hicode0101</a>
             </div>
         </n-gi>
     </n-grid>
 
+    <n-modal v-model:show="showModal" preset="card" style="width: 600px; height: 550px;">
+        <template #header>
+        <div>
+            
+            <n-tag>
+                {{ detailItem.name }}
+            <template #icon>
+                <n-icon :component="ChromeIcon" v-model:color="detailItem.color" />
+            </template>
+            </n-tag>
+            
+        </div>
+        </template>
+        <div>
+            <n-table >
+                <tbody>
+                    <tr>
+                        <td colspan="2">
+                            <n-alert type="info">
+                                不经过代理连接的主机列表: (每行一个主机)
+                            </n-alert>
+                        </td>
+                        
+                    </tr>
+                    <tr>
+                        <td>ByPassHost：</td>
+                        <td>
+                            
+                            <n-input
+                                v-model:value="detailBypassHost"
+                                placeholder="请输入 主机IP 或 域名通配符"
+                                type="textarea"
+                                :autosize="{
+                                    minRows: 5,
+                                    maxRows: 5,
+                                }"  :style="{ width: '360px',margin: '0 5px' }"
+                                />  
+                        </td>
+                    </tr>
+                </tbody>
+            </n-table>
+            
+            <n-table v-show="true">
+                <tbody>
+                    <tr>
+                        <td colspan="2">
+                            <n-alert type="info">
+                                代理登录: (如无需密码登录，则下面选项全部留空)
+                            </n-alert>
+                        </td>
+                        
+                    </tr>
+                    
+                    <tr>
+                        <td>UserName：</td>
+                        <td>
+                            <n-input v-model:value="tmpMsg" type="text" placeholder="用户名" :style="{ width: '200px',margin: '0 5px' }" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>PassWord：</td>
+                        <td>
+                            <n-input v-model:value="tmpMsg" type="text" placeholder="密码" :style="{ width: '200px',margin: '0 5px' }" />
+                        </td>
+                    </tr>
+                </tbody>
+            </n-table>
+            
+        </div>
+        <template #action>
+        <div>
+            
+            <n-popover trigger="hover">
+                <template #trigger>
+                    <n-button strong secondary round type="info" @click="detailToList">
+                    确定修改，并返回到列表
+                    </n-button>
+                </template>
+                <span>修改后在列表界面，点批量保存按钮生效</span>
+            </n-popover>
+        </div>
+        </template>
+    </n-modal>
+  
 </template>
 
 <script lang="ts" setup>
 import { defineComponent, h, reactive, ref } from 'vue'
 import { useDialog, useMessage } from 'naive-ui'
 
+import {
+    LogoChrome as ChromeIcon
+} from '@vicons/ionicons5'
+
+import { AppsListDetail24Regular as DetailIcon } from '@vicons/fluent'
+
+
 const message = useMessage()
 const dialog = useDialog()
+
+let proxyConfigList: any = ref([]);
+
+let showModal = ref(false);
+let detailItem = ref({});
+let detailBypassHost = ref("");
+
+let tmpMsg = ref("");
 
 function openPopup() {
     window.location.href = "./popup.html";
@@ -127,7 +241,7 @@ let proxyTypeOptions = ['HTTP', 'HTTPS', 'SOCKS4', 'SOCKS5'].map(
         })
       );
 
-let proxyConfigList: any = ref([]);
+
 
 
 browser.storage.local.get(["proxyConfigs"]).then((result: any) => {
@@ -137,11 +251,11 @@ browser.storage.local.get(["proxyConfigs"]).then((result: any) => {
         console.log("GetValue is undefined");
         
     } else {
-        console.log("get storage:",result.proxyConfigs);
+        //console.log("get storage:",result.proxyConfigs);
         let proxyConfigs = new Map(JSON.parse(result.proxyConfigs));
         console.log("map is ", proxyConfigs);
         proxyConfigs.forEach((proxyConfig, key) => {
-            console.log(key + " = " + proxyConfig);
+            //console.log(key + " = " + proxyConfig);
             proxyConfigList.value.push(proxyConfig);
         });
 
@@ -156,6 +270,11 @@ async function saveConfigs() {
     //message.info("saveConfigs ...");
 
     //console.log(proxyConfigList.value);
+
+    if(!checkForName()){
+        message.error("存在重复的配置名，请检查");
+        return;
+    }
     
     let proxyConfigsMap = new Map<string,any>();
     
@@ -173,7 +292,7 @@ async function saveConfigs() {
     
     //setTimeout(() => {refreshPage();},2000);
 
-    message.success('保存成功',{duration:2000, onAfterLeave: () => {refreshPage();}});
+    message.success('保存成功！即将刷新页面...',{duration:2000, onAfterLeave: () => {refreshPage();}});
     
 }
 
@@ -203,6 +322,83 @@ function refreshPage() {
   window.location.reload();
 }
 
+function editDetail(item : any, event : Event){
+    showModal.value = true;
+    
+    console.log(item);
+    detailItem.value = item;
+    console.log(detailItem);
+    
+    detailBypassHost.value = item.rules.bypassList.join("\n");
+}
+
+function detailToList(){
+    //console.log(detailBypassHost.value);
+    let _pid = detailItem.value.pid;
+    let _bypassList = detailBypassHost.value.split("\n");
+    console.log(_pid, _bypassList);
+
+    
+    proxyConfigList.value.forEach((element: any) => {
+        if (element.pid == _pid) {
+            element.rules.bypassList = _bypassList;
+        }
+    });
+
+    //console.log(proxyConfigList);
+    
+    //
+    detailItem.value = {};
+    detailBypassHost.value = "";
+    
+    showModal.value = false;
+    
+    
+}
+
+function keyUpNameEvent(item : any, event : Event){
+    //message.info('[Event keyup]')
+    console.log("keyup:",item.pid, item.name);
+    
+
+    
+}
+
+function changeNameEvent(item : any, event : Event){
+    //message.info(`[Event change]: ${item.name}`)
+    
+    console.log("change:",item.pid, item.name);
+    
+    //console.log(checkForName());
+    if(!checkForName()){
+        message.error("存在重复的配置名，请检查");
+    }
+}
+
+function checkForName() {
+    let result = true;
+    proxyConfigList.value.forEach((elementA: any) => {
+        let _name = elementA.name;
+        let _pid = elementA.pid;
+        let _count = 0;
+        proxyConfigList.value.forEach((elementB: any) => {
+        
+            if (elementB.name == _name) {
+                _count++;
+
+                if (_count >= 2) {
+                    
+                    result = false;
+                }
+            }
+
+            
+        });
+    });
+
+    return result;
+}
+
 </script>
 
 <style scoped>
@@ -216,6 +412,7 @@ function refreshPage() {
 
     background-color: rgba(244, 244, 244, 0.24);
     min-height: 60vh;
+    margin: 0 5px;
 }
 
 .btnBlock {
